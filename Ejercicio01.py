b797cc3c771f2a42a225e5bbe44235b3585badd5
sgimenelan <sgimenelan@educacion.navarra.es>
2024-12-16 11:26:05 +0100

    Update Ejercicio01.py

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -1,4 +1,8 @@
+def tablamultiplicar(n):
+    nombre = ("tabla-" + str(n) + ".txt")
+    with open(nombre, "w") as file:
+        for i in range(1, 11):
+            file.write(str(i)+ "x"+ str(n)+ "="+ str(i * n) + "\n")
+        return file
 n = int(input("Introduce un numero entero del 1 al 10: "))
-with open("tabla-" + str(n) + ".txt", "w") as file:
-    for i in range(1, 11):
-        file.write(str(i)+ "x"+ str(n)+ "="+ str(i * n) + "\n")
+tablamultiplicar(n)
