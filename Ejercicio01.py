d21e791a8bddc2947271e5034541523a375c6f55
sgimenelan <sgimenelan@educacion.navarra.es>
2024-12-16 11:30:44 +0100

    Update Ejercicio01.py

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -1,4 +1,11 @@
 def tablamultiplicar(n):
+    """La funcion recibe un número y crea un fichero con la 
+    tabla de multiplicar del mismo
+    Parámetros:
+...   - numero: Un numero entero del 1 al 10
+... Salida:
+      - el fichero con la tabla de multiplicar """
+
     nombre = ("tabla-" + str(n) + ".txt")
     with open(nombre, "w") as file:
         for i in range(1, 11):
