5d29facf87602a88f3cc3df1429c017d4f1ba67c
sgimenelan <sgimenelan@educacion.navarra.es>
2024-12-16 12:04:26 +0100

    Create Ejercicio06.py

---
+++ Ejercicio06.py
@@ -0,0 +1,17 @@
+#Escribir un programa para gestionar un listín telefónico con los 
+#nombres y los teléfonos de los clientes de una empresa. 
+#El programa debe  incorporar funciones para crear el fichero
+#con el listín si no existe, para consultar el teléfono de un
+#cliente, añadir el teléfono de un nuevo cliente y eliminar el 
+#teléfono de un cliente. El listín debe estar guardado en el fichero
+#de texto listin.txt donde el nombre del cliente y su teléfono
+#deben aparecer separados por comas y cada cliente en una línea distinta.
+"""def crear(nombre):
+
+def numerotelefono(nombre):
+
+def añadirtelefono(nombre):
+
+def eliminartelefono(nombre):"""
+
+menu = int(input("(1)Crear listín \n(2)Consultar telefono \n(3)Añadir cliente \n(4)Eliminar cliente \n"))
