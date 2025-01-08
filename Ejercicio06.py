5739d16892254cfd443d8d3b656bc42eca13c42c
sgimenelan <sgimenelan@educacion.navarra.es>
2025-01-08 19:22:37 +0100

    Update Ejercicio06.py

--- Ejercicio06.py
+++ Ejercicio06.py
@@ -6,12 +6,66 @@
 #teléfono de un cliente. El listín debe estar guardado en el fichero
 #de texto listin.txt donde el nombre del cliente y su teléfono
 #deben aparecer separados por comas y cada cliente en una línea distinta.
-"""def crear(nombre):
+import os
+
+def crear():
+    if not os.path.isfile("listin.txt"):
+        with open("listin.txt", "w") as file:
+            print("Fichero creado correctamente.")
+    else:
+        print("El fichero ya existe.")
 
 def numerotelefono(nombre):
+    if os.path.isfile("listin.txt"):
+        with open("listin.txt", "r") as file:
+            clientes = file.readlines()
+            for cliente in clientes:
+                if cliente.startswith(nombre + ","):
+                    print(f"Teléfono de {nombre}: {cliente.split(',')[1].strip()}")
+                    return
+            print(f"Cliente {nombre} no encontrado.")
+    else:
+        print("El fichero listin.txt no existe.")
 
-def añadirtelefono(nombre):
+def anadirtelefono(nombre, telefono):
+    with open("listin.txt", "a") as file:
+        file.write(f"{nombre},{telefono}\n")
+    print(f"Cliente {nombre} añadido con éxito.")
 
-def eliminartelefono(nombre):"""
+def eliminartelefono(nombre):
+    if os.path.isfile("listin.txt"):
+        with open("listin.txt", "r") as file:
+            clientes = file.readlines()
+        with open("listin.txt", "w") as file:
+            eliminado = False
+            for cliente in clientes:
+                if not cliente.startswith(nombre + ","):
+                    file.write(cliente)
+                else:
+                    eliminado = True
+            if eliminado:
+                print(f"Cliente {nombre} eliminado.")
+            else:
+                print(f"Cliente {nombre} no encontrado.")
+    else:
+        print("El fichero listin.txt no existe.")
 
-menu = int(input("(1)Crear listín \n(2)Consultar telefono \n(3)Añadir cliente \n(4)Eliminar cliente \n"))
+
+menu=""
+while True:
+    menu = int(input("(1)Crear listín \n(2)Consultar telefono \n(3)Añadir cliente \n(4)Eliminar cliente \n"))
+    if menu == 1:
+        print("***Creando listin...***")
+        crear()
+
+    if menu == 2:
+        print("***Consultar telefono***")
+        numerotelefono(input("Dime el nombre: "))
+
+    if menu == 3:
+        print("***Añadir cliente***")
+        anadirtelefono((input("Dime el nombre: ")),(input("Dime el telefono:")))
+
+    if menu == 4:
+        print("***Eliminar cliente***")
+        eliminartelefono(input("Dime el nombre: "))
